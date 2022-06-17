70bff9b4a364cfc0b2a32f462d31c0483b58c16c
Scott Chow <scottchowww@gmail.com>
2022-06-17 18:29:02 +0800

    added alternative solution

---
+++ num_bits/alternative.c
@@ -0,0 +1,21 @@
+// this operation: n & (n - 1) removes the rightmost bit with the value 1. Thus, the number of operations it take to make a number completely zero is also the number of '1' bits in the number.
+
+// n = 01011100
+// 1st op = 01011000
+// 2nd op = 01010000
+// 3rd op = 01000000
+// 4th op = 00000000
+// since we performed the operation 4 times, we can say that there are 4 '1' bits in the number.
+
+int	hammingWeight(unsigned int n)
+{
+	int	sum;
+
+	sum = 0;
+	while (n)
+	{
+		n = n & (n - 1);
+		sum++;
+	}
+	return (sum);
+}
